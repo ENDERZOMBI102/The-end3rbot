3478f1aec838d48dfb603c76af95d5306db4f8df
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-06-07 22:46:09 +0200

    command examples

---
+++ example.py
@@ -0,0 +1,36 @@
+"""
+a command object that contains all the possible options
+    {
+        "send": "",
+        "press": "",
+        "varIsPing": true,
+        "needVar": true,
+        "paramReplace": "",
+        "canBeUsedBy": "", # everyone, mod, streamer
+        "data": {
+            "url": "",
+            "urljson": "",
+            "sections": [],
+            "load" : "varname",
+            "saveAs" : ["varName", "url"]
+        }
+    }
+
+this is an example implementing a ban command
+{
+    "send": "/ban {}",
+    "varIsMention": true, // this can be omitted, this makes the command strip the @
+    "needVar": true,
+    "paramReplace" : "{}"
+}
+this is an example implementing a "whats the latest version of" command
+{
+    "send": "latest version of {0} is: {}",
+    "paramReplace" : "{0}",
+    "needVar" : true,
+    "data": {
+        "urljson": "paramVar",
+        "sections": ["tag_name"]
+    }
+}
+"""
