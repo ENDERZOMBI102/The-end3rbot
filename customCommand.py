811da91ccb1d8fef3aec1671e0ffb4ec5e5f31bf
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-06-08 23:29:33 +0200

    almost finished

--- customCommand.py
+++ customCommand.py
@@ -1,20 +1,22 @@
 import json
+import twitch
 import keyboard
 import requests as req
-from bot import chat
+
+chat: twitch.Chat
 
 
 customCommandsData = {}
-customCommands = {
+commandList = {
     'exampleCommand' : {
         'text' : 'this is an example {url}',
-        'get' : {
+        'data' : {
             'url' : 'https://raw.githubusercontent.com/ENDERZOMBI102/ucpDatabase/master/.gitignore'
         }
     },
     'exampleCommand2' : {
         'text' : 'this is another example {}',
-        'get' : {
+        'data' : {
             'urljson' : '',
             'sections': ['first', 'second']
         }
@@ -22,7 +24,13 @@ customCommands = {
 }
 
 
-def addCustomCommand( strdata: str ) -> None:
+def init(c: twitch.Chat):
+    chat = c
+    with open('./commands.json', 'r') as file:
+        commandList = json.load(file)
+
+
+def add( strdata: str ) -> None:
     try:
         data: dict = json.loads(strdata)
     except:
@@ -35,7 +43,7 @@ def addCustomCommand( strdata: str ) -> None:
     del strdata
     chat.send('checking syntax..')
     # check if it is already defined before checking the command
-    if data['name'] in customCommands:
+    if data['name'] in commandList.keys():
         chat.send('you can\'t overwrite a command!')
         return
     # this section check the command syntax
@@ -92,18 +100,18 @@ def addCustomCommand( strdata: str ) -> None:
                 return
     name = data['name']  # save the command name
     del data['name']  # delete the name key
-    customCommands[name] = data  # create the command under the command name
+    commandList[name] = data  # create the command under the command name
     with open('./commands.json', 'w') as file:  # save all in the config file
-        json.dump(customCommands, file, indent=4)
+        json.dump(commandList, file, indent=4)
     chat.send(f'command "{name}" successfully added')  # send success notice
 
 
 # this is the function that take care of command execution
 # TODO: order please!
 # TODO: add key pressing as action
-def customCommand( comDict: dict = None, variable: str = None ):
+def execute( command: str, variable: str = None ):
     """
-    :param comDict: command to execute
+    :param command: command name to execute
     :param variable: command variable
     """
     """
@@ -115,6 +123,8 @@ def customCommand( comDict: dict = None, variable: str = None ):
             return # 'return because its an error, cannot continue
     
     """
+    comDict: dict = commandList[command]
+    del command
     # if the command should have a parameter and it doesn't, send an error
     # variable checks
     if 'needVar' in comDict.keys():  # check if the command needs a variable
@@ -133,36 +143,58 @@ def customCommand( comDict: dict = None, variable: str = None ):
                 return  # no
     # data operations
     #  url action
-    if 'url' in comDict['data'].keys():
-        try:
-            comDict['send'] = comDict['send'].replace('{url}', req.get(comDict['data']['url']).text)
-        except Exception as e:
-            chat.send(f'An error occurred while processing "url": {e}')
-            return  # error!
-    #  urljson action
-    if 'urljson' in comDict['data'].keys():
-        try:
-            data = req.get(comDict['data']['url']).json()
-        except Exception as e:
-            chat.send(f'An error occurred while processing "urljson": {e}')
-            return  # error!
-        if len(comDict['data']['sections']) is 1:  # 1 section
-            comDict['send'] = comDict['send'].replace(  # replace
-                '{urljson}',  # what to replace
-                data[comDict['data']['sections'][0]]  # replace with
-            )
-        else:  # two sections
-            comDict['send'] = comDict['send'].replace(  # replace
-                '{urljson}',  # what to replace
-                data[comDict['data']['sections'][0]][comDict['data']['sections'][1]]  # replace with
-            )
-    # load action
-    if 'load' in comDict['data'].keys():
-        try:
-            comDict['send'] = comDict['send'].replace(
-                '{var}',
-                customCommandsData[comDict['data']['load']]
-            )
-        except:
-            chat.send(f'An error occurred while processing "load": no variable named {comDict["data"]["load"]}')
-            return  # error!
+    if 'data' in comDict.keys():
+        if 'url' in comDict['data'].keys():
+            try:
+                comDict['send'] = comDict['send'].replace('{url}', req.get(comDict['data']['url']).text)
+            except Exception as e:
+                chat.send(f'An error occurred while processing "url": {e}')
+                print(comDict)
+                return  # error!
+        #  urljson action
+        if 'urljson' in comDict['data'].keys():
+            try:
+                data = req.get(comDict['data']['url']).json()
+            except Exception as e:
+                chat.send(f'An error occurred while processing "urljson": {e}')
+                return  # error!
+            if len(comDict['data']['sections']) == 1:  # 1 section
+                comDict['send'] = comDict['send'].replace(  # replace
+                    '{urljson}',  # what to replace
+                    data[comDict['data']['sections'][0]]  # replace with
+                )
+            else:  # two sections
+                comDict['send'] = comDict['send'].replace(  # replace
+                    '{urljson}',  # what to replace
+                    data[comDict['data']['sections'][0]][comDict['data']['sections'][1]]  # replace with
+                )
+        # load action
+        if 'load' in comDict['data'].keys():
+            try:
+                comDict['send'] = comDict['send'].replace(
+                    '{var}',
+                    customCommandsData[comDict['data']['load']]
+                )
+            except:
+                chat.send(f'An error occurred while processing "load": no variable named {comDict["data"]["load"]}')
+                return  # error!
+        # saveAs action
+        if 'saveAs' in comDict['data'].keys():
+            saveData: list = comDict['data']['saveAs']  # for semplicity
+            if len(saveData) == 1:
+                customCommandsData[saveData[0]] = variable
+            elif len(saveData) == 2:
+                customCommandsData[saveData[0]] = req.get(saveData[1]).text
+            else:
+                chat.send('too many values for "saveAs"! no variable has been saved!')
+                return
+            del saveData  # no survivors :)
+    # paramReplace action
+    if 'paramReplace' in comDict.keys():
+        comDict['send'] = comDict['send'].replace(comDict['paramReplace'], variable)
+    # press action
+    if 'press' in comDict.keys():
+        keyboard.press_and_release(comDict['press'])
+    # send action
+    if 'send' in comDict.keys():
+        chat.send(comDict['send'])
