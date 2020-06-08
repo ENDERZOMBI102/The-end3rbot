d2418f0a98264bb60df53f8a9cea94fe6e350908
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-06-08 23:29:52 +0200

    now with 100% more bugs

--- bot.py
+++ bot.py
@@ -3,15 +3,14 @@ import keyboard
 import asyncio
 import json
 import os
-from customCommand import *
+import customCommand
 import dotenv
 
 
 symbol = '!'
 timeout = False
 counter: int = 0
-chat: twitch.Chat
-user: str = '#enderzombi102'
+chat: twitch.Chat = None
 
 
 def handler(message: twitch.chat.Message):
@@ -27,24 +26,28 @@ def handler(message: twitch.chat.Message):
     # if there's a @ has a mentions
     hasPing = '@' in text
     # is a custom command?
-    isCustom = command in customCommands
+    isCustom = command in customCommand.commandList
     # print command infos
     print(f' command: {command}, variable: {variable}, has ping: {hasPing}, is custom: {isCustom}')
     if isCustom:
         # is a custom command, execute it with the cc handler
-        customCommand(customCommands[command], variable)
+        customCommand.execute(command, variable)
         return
     # not a custom command, use normal handler
-    if command == 'pause' and os.getenv('CHANNEL') is '#enderzombi102':
+    elif command == 'pause' and os.getenv('CHANNEL') is '#enderzombi102':
         if not timeout: # press esc!
             asyncio.run( waitForTimeout() )
             keyboard.press_and_release('esc')
         else:
             # timer is still running!
             chat.send(f'can\' run that! wait {counter} seconds more!')
-    if command == 'addCommand':
+    elif command == 'addCommand':
         # add a custom command
-        addCustomCommand(variable)
+        customCommand.add(variable)
+
+    elif command == 'minecraft':
+        chat.send('ikr, tuff should play minecraft!')
+
 
 async def waitForTimeout():
     global timeout
@@ -63,12 +66,12 @@ async def count(seconds: int = 10):
 
 if __name__ == '__main__':
     dotenv.load_dotenv()
-    chat = twitch.Chat(channel=os.getenv('CHANNEL').lower(),
-                       nickname=os.getenv('USERNAME'),
-                       oauth=os.getenv('OAUTH_TOKEN')
-                       )
+    chat = twitch.Chat(
+        channel=os.getenv('CHANNEL_1').lower(),
+        nickname=os.getenv('USERNAME'),
+        oauth=os.getenv('OAUTH_TOKEN')
+    )
     chat.subscribe(handler)
-    with open('./commands.json', 'r') as file:
-        customCommands = json.load(file)
+    customCommand.init(chat)
     print('ready!')
     chat.send('The end3rbot successfully connected')
