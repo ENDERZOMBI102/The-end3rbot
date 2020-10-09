38d93bca8c03f37a436ef59ecb827a02eec6e810
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-10-09 20:58:26 +0200

    removed attributes from the dict

--- TwitchBotV2/utils.py
+++ TwitchBotV2/utils.py
@@ -1,16 +1,9 @@
-
-
 def createChannelDict(channel: str) -> dict:
-        channel = channel.lower()
-        return {
-            {
-                'channel' : f'#{channel}' if '#' not in channel else channel,
-                'hasOtherBots' : False,
-                'prefix' : '!',
-                'moderators' : [],
-                'operators'  : [
-                    channel
-                ],
-                'customCommands' : {}
-            }
-        }
+	channel = channel.lower()
+	return {
+		'channel': channel if '#' not in channel else channel.replace('#', '', 1),
+		'hasOtherBots': False,
+		'prefix': '!',
+		'operators': [],
+		'commands': {}
+	}
